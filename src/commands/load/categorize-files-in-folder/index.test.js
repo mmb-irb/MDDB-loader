fe5f9cb62c4949e6718e65f9195a365e18b0d20a
Dani Beltrán <daniel.beltran@irbbarcelona.org>
2020-12-11 18:54:04 +0100

    test maintenance

--- src/commands/load/categorize-files-in-folder/index.test.js
+++ src/commands/load/categorize-files-in-folder/index.test.js
@@ -5,17 +5,19 @@ describe('categorizeFilesInFolder', () => {
     const files = await categorizeFilesInFolder(`${__dirname}/__fixtures`);
     expect(files).toEqual({
       allFiles: [
+        'md.analysis.xvg',
         'md.dcd',
         'md.imaged.rot.xtc',
         'md.pca.eigenval.xvg',
         'md.pdb',
-        'md.xvg',
+        'metadata.json',
         'some-file',
       ],
+      metadataFile: 'metadata.json',
       pcaFiles: ['md.pca.eigenval.xvg'],
       rawFiles: ['md.imaged.rot.xtc', 'md.pdb'],
       trajectoryFiles: ['md.imaged.rot.xtc'],
-      analysisFiles: ['md.xvg'],
+      analysisFiles: ['md.analysis.xvg'],
     });
   });
 });
